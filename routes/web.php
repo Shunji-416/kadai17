29cdfa2eea9cd5c2cc0a69131dd8f555cfad6b77
Shunji Ito <itoshun27@icloud.com>
2021-08-09 12:18:05 +0000

    kadai

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 //課題4
@@ -25,3 +25,7 @@ Route::get('admin/profile/create',
 
 Route::get('admin/profile/edit',
 'Profile\Controller@edit');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
